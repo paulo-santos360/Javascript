8a41053f3904a7283b5fa8de6f8ca750c7e2c0a2
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-07-10 08:46:39 -0300

    atualizar

---
+++ Javascript-Variavel/js-variavel-let-const/index.js
@@ -0,0 +1,14 @@
+//Let vs Const Vs Var
+
+//usando var
+var value = "some";
+
+//usando let
+var name = "john";
+
+//usando const (const)
+const lastName = "Jordan";
+
+console.log(value);
+console.log()
+
