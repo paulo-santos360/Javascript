0de43bfb7887b018e325b300e7447d0433078e98
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-07-10 09:34:41 -0300

    atualizar

---
+++ Javascript-Variavel-Let-Const/js-Diferencas-VAR-LET-CONST/index.js
@@ -0,0 +1,23 @@
+const verdadeira = true;
+
+
+let nome = 'Luiz';
+//let não pode ser redeclarada
+
+var nome2 = 'Luiz';
+//var pode ser redeclarada
+
+let nome = 'ZE';
+
+/* if(verdadeira){
+    console.log(verdadeira)
+} */
+
+/* if(verdadeira){
+    console.log(nome, nome2);
+} */
+
+/* if(verdadeira){
+    let nome = 'NARUTO'
+    console.log(nome, nome2);
+} */
