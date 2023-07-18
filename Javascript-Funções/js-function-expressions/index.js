e207f0f05ad72fc561b24460275bdc05cd347ab7
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-07-18 14:07:23 -0300

    atualizar

---
+++ Javascript-Funções/js-function-expressions/index.js
@@ -0,0 +1,10 @@
+function addValues(num1, num2){
+    return num1 + num2
+}
+
+const firstValue = addValues(3, 4);
+const secondValue = addValues(12, 34);
+// function expression
+
+const values = [firstValue, secondValue];
+console.log(values);
