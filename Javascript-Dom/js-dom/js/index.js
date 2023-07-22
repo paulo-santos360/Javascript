95080fe150f5c9285c8103da26ee0895a00149b9
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-07-22 17:30:41 -0300

    atualizar

---
+++ Javascript-Dom/js-dom/js/index.js
@@ -0,0 +1,9 @@
+//document.querySelector('#btn').style.color = 'red';
+
+//document.querySelector('body').style.background = 'red';
+
+document.body.style.background = 'yellow';
+document.body.style.color = 'red';
+document.getElementById('btn').style.color = 'green';
+
+console.log(window);
