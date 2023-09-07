edafd7fe5208e07b00ed6771a506c8985e11039e
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-09-07 08:47:19 -0300

    Javascript string

---
+++ Javascript-Array/js-string-template/js/index.js
@@ -0,0 +1,10 @@
+// Template Literals - ES6+
+// Backtick characters `` - above tab (left from one)
+// Interpolation ${} - insert expression(value)
+
+const name = 'john';
+const age = 25;
+const sentence = "Hey is 's " + name + ' and is ' + age + ' years old';
+
+//console.log(name)
+document.getElementById('output').textContent = sentence;
