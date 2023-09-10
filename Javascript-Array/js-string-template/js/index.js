e35d752fb6ec61426a435dafaa0988eb4756663a
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-09-10 19:32:54 -0300

    Javascript

--- Javascript-Array/js-string-template/js/index.js
+++ Javascript-Array/js-string-template/js/index.js
@@ -6,5 +6,8 @@ const name = 'john';
 const age = 25;
 const sentence = "Hey is 's " + name + ' and is ' + age + ' years old';
 
+const value = `Hey it 's ${name}  and he is ${age}  years old. And here is some simple math ${3 + 7}`;
+
 //console.log(name)
 document.getElementById('output').textContent = sentence;
+document.getElementById('output').textContent = value;
